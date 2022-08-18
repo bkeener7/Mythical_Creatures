61066f4fc7f7e1d3062c78ebe6f3d9a4d7532839
Bryan Keener <bkeener7@gmail.com>
2022-08-18 13:31:23 -0600

    complete medusa exercise

--- mythical-creatures/spec/medusa_spec.rb
+++ mythical-creatures/spec/medusa_spec.rb
@@ -32,10 +32,37 @@ RSpec.describe Medusa do
   end
 
   it 'can only have three victims' do
-    # your code here
+    medusa = Medusa.new('Cassiopeia')
+    victim1 = Person.new('Jim')
+    victim2 = Person.new('Jimbo')
+    victim3 = Person.new('Jimbob')
+    victim4 = Person.new('Frank')
+
+    medusa.stare(victim1)
+    medusa.stare(victim2)
+    medusa.stare(victim3)
+    medusa.stare(victim4)
+
+    expect(medusa.statues.count).to eq(3)
+
   end
 
   it 'if a fourth victim is stoned the first is unstoned' do
-    # your code here
+    medusa = Medusa.new('Cassiopeia')
+    victim1 = Person.new('Jim')
+    victim2 = Person.new('Jimbo')
+    victim3 = Person.new('Jimbob')
+    victim4 = Person.new('Frank')
+
+    medusa.stare(victim1)
+    medusa.stare(victim2)
+    medusa.stare(victim3)
+    medusa.stare(victim4)
+
+    expect(medusa.statues.count).to eq(3)
+    expect(victim1.stoned?).to be false
+    expect(victim2.stoned?).to be true
+    expect(victim3.stoned?).to be true
+    expect(victim4.stoned?).to be true
   end
 end
